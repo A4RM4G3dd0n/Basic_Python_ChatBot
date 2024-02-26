7e841dbe91f5664ac40dcc6fb4ccd113aef28da7
A4RM4G3dd0n <148641533+A4RM4G3dd0n@users.noreply.github.com>
2024-02-26 16:44:52 +0200

    Create script.py
    A super simple chatbot

---
+++ script.py
@@ -0,0 +1,12 @@
+print("Welcome to a basic Python chatbot! ")
+
+stuff_to_echo = input("Enter something to repeat: ")
+
+print("You said: " + stuff_to_echo)
+
+what_is_name = input("Type in your name: ")
+print("Hello " + what_is_name + "!")
+
+favorite_fruit = input("Enter your favorite fruit: ")
+
+print("Incredible! this time of year it is "+favorite_fruit+ " season")
